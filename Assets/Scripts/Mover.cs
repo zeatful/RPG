5bea9294ee95058b80570541fbe8f29921571be5
Eric Foster <efoster@harriscomputer.com>
2020-02-15 15:33:01 -0500

    first push

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    // player target to navigate towards
+    [SerializeField] Transform target;
+
+    // Last ray shot from camera
+    Ray lastRay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}
